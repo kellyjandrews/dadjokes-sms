d5edc86664c176e29f66345cb231f25ad8c7d7c4
Kelly J Andrews <kelly@kellyjandrews.com>
2019-04-10 14:20:49 -0400

    init

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:11.8.0-alpine as dev-stage
+
+MAINTAINER Kelly J Andrews <kelly@kellyjandrews.com>
+
+WORKDIR /usr/src
+
+COPY package.json .
+
+RUN npm install --quiet
+
+ENV PATH /usr/src/node_modules/.bin:$PATH
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ['npm', 'start']
+# FROM node:11.8.0-alpine as build-stage
+#
+# WORKDIR /usr/src
+# COPY --from=dev-stage /usr/src /usr/src
+# RUN npm run build
+#
+# FROM nginx:1.15
+# COPY --from=build-stage /usr/src/build/ /usr/share/nginx/html
+# COPY --from=build-stage /usr/src/nginx.conf /etc/nginx/conf.d/default.conf
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('superagent');
+const Nexmo = require('nexmo');
+
+const app = express();
+const port = 3000;
+
+const nexmo = new Nexmo({
+    apiKey: 'NEXMO_KEY',
+    apiSecret: '61XuKoPBEX6WGrhI'
+  });
+
+app.use(bodyParser.json());
+
+app.get('/up', (req, res) => {
+  res.sendStatus(200);
+};
+
+app.post('/sms', (req, res) => {
+  let message;
+  if (req.body.keyword === "JOKE") {
+    request
+      .get('https://icanhazdadjoke.com/')
+      .set('accept', 'application/json')
+      .then((res) => {
+        nexmo.message.sendSms(req.body.to, req.body.msisdn, res.body.joke, (err, res) =>{
+          console.log(res);
+        });
+      });
+  } else {
+    nexmo.message.sendSms(req.body.to, req.body.msisdn, 'Did not recognize that keyword', (err, res) =>{
+      console.log(res);
+    });
+  }
+  res.sendStatus(200);
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
